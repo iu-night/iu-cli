22488af740bae81ac7915af2a58976bd19cf643a
iu-night <iu.git@outlook.com>
2023-01-06 16:59:36 +0800

    Comment out some useless codes

--- src/frameworks.ts
+++ src/frameworks.ts
@@ -25,43 +25,43 @@ export const FRAMEWORKS: Framework[] = [
         display: 'TypeScript',
         color: c.blue,
       },
-      {
-        name: 'custom-create-vue',
-        display: 'Customize with create-vue ↗',
-        color: c.green,
-        customCommand: 'npm create vue@latest TARGET_DIR',
-      },
-      {
-        name: 'custom-nuxt',
-        display: 'Nuxt ↗',
-        color: c.green,
-        customCommand: 'npm exec nuxi init TARGET_DIR',
-      },
+      // {
+      //   name: 'custom-create-vue',
+      //   display: 'Customize with create-vue ↗',
+      //   color: c.green,
+      //   customCommand: 'npm create vue@latest TARGET_DIR',
+      // },
+      // {
+      //   name: 'custom-nuxt',
+      //   display: 'Nuxt ↗',
+      //   color: c.green,
+      //   customCommand: 'npm exec nuxi init TARGET_DIR',
+      // },
     ],
   },
-  {
-    name: 'react',
-    display: 'React',
-    color: c.cyan,
-    variants: [
-      {
-        name: 'react-swc-ts',
-        display: 'TypeScript + SWC',
-        color: c.blue,
-      },
-    ],
-  },
-  {
-    name: 'others',
-    display: 'Others',
-    color: c.white,
-    variants: [
-      {
-        name: 'create-vite-extra',
-        display: 'create-vite-extra ↗',
-        color: c.white,
-        customCommand: 'npm create vite-extra@latest TARGET_DIR',
-      },
-    ],
-  },
+  // {
+  //   name: 'react',
+  //   display: 'React',
+  //   color: c.cyan,
+  //   variants: [
+  //     {
+  //       name: 'react-swc-ts',
+  //       display: 'TypeScript + SWC',
+  //       color: c.blue,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'others',
+  //   display: 'Others',
+  //   color: c.white,
+  //   variants: [
+  //     {
+  //       name: 'create-vite-extra',
+  //       display: 'create-vite-extra ↗',
+  //       color: c.white,
+  //       customCommand: 'npm create vite-extra@latest TARGET_DIR',
+  //     },
+  //   ],
+  // },
 ]
